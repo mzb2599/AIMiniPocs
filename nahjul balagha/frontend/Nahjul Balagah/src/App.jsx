ce55d16c87590fe0a378aa2c7ffead9fde7c6b1b
mzaki2599 <mzaki2599@gmail.com>
2026-02-20 23:26:48 +0530

    Frontend UI changes

--- nahjul balagha/frontend/Nahjul Balagah/src/App.jsx
+++ nahjul balagha/frontend/Nahjul Balagah/src/App.jsx
@@ -3,13 +3,28 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import NahjulBalagha from "./NahjulBalagah";
-
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <NahjulBalagha />
+      <BottomNavigation
+        showLabels
+        value={"value"}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </>
   );
 }
---
+++ nahjul balagha/frontend/Nahjul Balagah/src/Nahjul Balagah/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header
+      style={{
+        textAlign: "center",
+        color: "#D4AF37",
+      }}
+    >
+      <h1>Nahjul Balagah</h1>
+    </header>
+  );
+};
+
+export default Header;
